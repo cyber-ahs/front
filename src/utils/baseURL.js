29fefe1f92e7917455af054047bd92dd3c3fb858
cyber-ahs <alihassam@Alis-MacBook-Air.local>
2022-06-26 01:42:26 -0700

    3th

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,2 +1,4 @@
-const baseUrl = "http://alihassam1.herokuapp.com";
+const baseUrlLocal = "http://alihassam1.herokuapp.com";
 
+const baseUrl = process.env.REACT_APP_API_URL;
+export default baseUrl;
